5845b1f64e0a92b36fa8af419d34e5895fc9a630
Rlinger <sifenxinzi@163.com>
2018-06-08 20:17:35 +0800

    李四删除

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -39,12 +39,5 @@ public class User {
         this.password = password;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
+
 }
